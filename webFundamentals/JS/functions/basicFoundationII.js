7bc374a20a285ed5f307f657a75565ec747559b4
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-20 19:02:10 -0500

    evensAndOdds

--- webFundamentals/JS/functions/basicFoundationII.js
+++ webFundamentals/JS/functions/basicFoundationII.js
@@ -1,61 +1,133 @@
 // Biggie Size - Given an array, write a function that changes all positive numbers in the array to the string "big".  Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1, "big", "big", -5].
 
-function biggie(arr){
-    for(i=0; i<arr.length; i++){
-        if(arr[i]>0){
-            arr[i]="big";
+function biggie(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[i] = "big";
         }
     }
     return arr;
 }
-console.log(biggie([-5,-4,-3,-2,-1,0,1,2,3,4,5]))
+console.log(biggie([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]))
 
 // Print Low, Return High - Create a function that takes in an array of numbers.  The function should print the lowest value in the array, and return the highest value in the array.
-function lowHigh(arr){
-    var low=arr[0];
-    var high=arr[0];
-    for(i=0;i<arr.length;i++){
-        if(arr[i]<low){
+function lowHigh(arr) {
+    var low = arr[0];
+    var high = arr[0];
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] < low) {
             low = arr[i];
         }
-        if(arr[i]>high){
+        if (arr[i] > high) {
             high = arr[i];
         }
     }
     console.log(low);
     return high;
 }
-console.log(lowHigh([1,2,3,4,5]));
+console.log(lowHigh([1, 2, 3, 4, 5]));
 
 // Print One, Return Another - Build a function that takes in an array of numbers.  The function should print the second-to-last value in the array, and return the first odd value in the array.
 
-function oneAnother(arr){
-    console.log(arr[arr.length-2]);
-    for(i=0;i<arr.length;i++){
-        if(arr[i]<0){
+function oneAnother(arr) {
+    console.log(arr[arr.length - 2]);
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 != 0) {
             return arr[i];
         }
     }
-    return "No negative numbers"
+    return "No odd numbers"
 }
-console.log(oneAnother([1,-2,3,4,5]));
+console.log(oneAnother([1, 2, 3, 4, 5]));
 
 // Double Vision - Given an array (similar to saying 'takes in an array'), create a function that returns a new array where each value in the original array has been doubled.  Calling double([1,2,3]) should return [2,4,6] without changing the original array.
 
-function double(arr){
-    newArr=[];
-    for(i=0;i<arr.length;i++){
-        newArr.push(arr[i]*2);
+function double(arr) {
+    newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2);
     }
     return newArr;
 }
-console.log(double([1,2,3,4,5]));
+console.log(double([1, 2, 3, 4, 5]));
 
 // Count Positives - Given an array of numbers, create a function to replace the last value with the number of positive values found in the array.  Example, countPositives([-1,1,1,1]) changes the original array to [-1,1,1,3] and returns it.
 
+function countPositive(arr) {
+    var count = null;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            count++;
+        }
+    }
+    arr[arr.length - 1] = count;
+    return arr;
+}
+console.log(countPositive([-3, -2, -1, 0, 1, 2, 3]));
 
 // Evens and Odds - Create a function that accepts an array.  Every time that array has three odd values in a row, print "That's odd!".  Every time the array has three evens in a row, print "Even more so!".
 
+//The following will let you know if only 3 in a row.
+function evensAndOdds(arr) {
+    var evenCount = 0;
+    var oddCount = 0;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            oddCount = 0;
+            evenCount = 0;
+        }
+        if (arr[i] < 0) {
+            oddCount++;
+            if (evenCount == 3) {
+                console.log("Even more so!");
+            }
+            evenCount = 0;
+        }
+        if (arr[i] > 0) {
+            evenCount++;
+            if (oddCount == 3) {
+                console.log("That's odd!");
+            }
+            oddCount = 0;
+        }
+    }
+    if (oddCount == 3) {
+        console.log("That's odd!");
+    }
+    if (evenCount == 3) {
+        console.log("Even more so!");
+    }
+}
+evensAndOdds([1, 2, 3, -1, -2, -3, 1, 2, 3, -1, -2, 1, 2, 3, 4, -1, -2, -3])
+
+//The following will let you know of every 3 in a row.
+function evensAndOdds(arr) {
+    var evenCount = 0;
+    var oddCount = 0;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            evenCount = 0;
+            oddCount = 0;
+        }
+        if (arr[i] < 0) {
+            oddCount++;
+            if (oddCount == 3) {
+                console.log("That's odd!");
+                oddCount = 0;
+            }
+            evenCount = 0;
+        }
+        if (arr[i] > 0) {
+            evenCount++;
+            if (evenCount == 3) {
+                console.log("Even more so!");
+                evenCount = 0;
+            }
+            oddCount = 0;
+        }
+    }
+}
+evensAndOdds([1, 2, 3, -1, -2, -3, 1, 2, 3, -1, -2, 1, 2, 3, 4, 5, 6, -1, -2, -3])
 
 // Increment the Seconds - Given an array of numbers arr, add 1 to every other element, specifically those whose index is odd (arr[1], arr[3], arr[5], etc).  Afterward, console.log each array value and return arr.
 
