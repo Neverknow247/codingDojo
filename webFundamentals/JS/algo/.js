cee4c6069925696482a29745d25733faf2417a3a
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-21 15:51:26 -0500

    algos

--- webFundamentals/JS/algo/.js
+++ webFundamentals/JS/algo/.js
@@ -3,24 +3,47 @@ function change(amount) {
     var d = 0;
     var n = 0;
     var p = 0;
-    amount = amount;
-    current = 0;
-    while (current <= amount - 25) {
+    while (amount >= 25) {
         q += 1;
-        current += 25;
+        amount -= 25;
     }
-    while (current <= amount - 10) {
+    while (amount >= 10) {
         d += 1;
-        current += 10;
+        amount -= 10;
     }
-    while (current <= amount - 5) {
+    while (amount >= 5) {
         n += 1;
-        current += 5;
+        amount -= 5;
     }
-    while (current < amount) {
+    while (amount > 1) {
         p += 1;
-        current += 1;
+        amount -= 1;
     }
     console.log("q= ", + q + ", d= " + d + ", n= " + n + ", p= " + p)
 }
 change(90)
+
+
+
+
+function makeChangeWithDollars(cents) {
+    var coins = {
+        "dollars": [100, 0],
+        "half dollars": [50, 0],
+        "quarters": [25, 0],
+        "dimes": [10, 0],
+        "nickels": [5, 0],
+        "pennies": [1, 0]
+    }
+    for (key in coins) {
+        while (cents >= coins[key][0]) {
+            coins[key][1]++
+            cents -= coins[key][0]
+        }
+    }
+    console.log(cents + " cents can be represented by:")
+    for (key in coins) {
+        console.log(key + ": " + coins[key][1])
+    }
+}
+makeChangeWithDollars(90)
--- webFundamentals/JS/functions/basicFoundationII.js
+++ webFundamentals/JS/functions/basicFoundationII.js
@@ -127,28 +127,100 @@ function evensAndOdds(arr) {
         }
     }
 }
-evensAndOdds([1, 2, 3, -1, -2, -3, 1, 2, 3, -1, -2, 1, 2, 3, 4, 5, 6, -1, -2, -3])
+evensAndOdds([1, 2, 3, -1, -2, -3, 1, 2, 3, -1, -2, 1, 2, 3, 4, 5, 6, -1, -2, -3, -4])
 
 // Increment the Seconds - Given an array of numbers arr, add 1 to every other element, specifically those whose index is odd (arr[1], arr[3], arr[5], etc).  Afterward, console.log each array value and return arr.
-
+console.log(incrementInSeconds([0, 1, 2, 3, 4, 5]));
+function incrementInSeconds(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (i % 2 != 0) {
+            arr[i] += 1;
+        }
+    }
+    return arr;
+}
 
 // Previous Lengths - You are passed an array (similar to saying 'takes in an array' or 'given an array') containing strings.  Working within that same array, replace each string with a number - the length of the string at the previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4]. Hint: Can for loops only go forward?
 
+console.log(previousLengths(["hello", "dojo", "awesome"]));
+function previousLengths(arr) {
+    for (i = arr.length - 1; i > 0; i--) {
+        arr[i] = arr[i - 1].length;
+    }
+    return arr;
+}
 
 // Add Seven - Build a function that accepts an array. Return a new array with all the values of the original, but add 7 to each. Do not alter the original array.  Example, addSeven([1,2,3]) should return [8,9,10] in a new array.
 
+console.log(addSeven([1, 2, 3]));
+function addSeven(arr) {
+    var newArr = [];
+    for (i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] + 7);
+    }
+    return newArr
+}
 
 // Reverse Array - Given an array, write a function that reverses its values, in-place.  Example: reverse([3,1,6,4,2]) returns the same array, but now contains values reversed like so... [2,4,6,1,3].  Do this without creating an empty temporary array.  (Hint: you'll need to swap values).
 
+console.log(reverseArr([1, 2, 3, 4, 5]));
+function reverseArr(arr) {
+    for (i = 0; i < arr.length / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[(arr.length - 1) - i];
+        arr[(arr.length - 1) - i] = temp;
+    }
+    return arr
+}
 
 // Outlook: Negative - Given an array, create and return a new one containing all the values of the original array, but make them all negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
 
+console.log(negative([-3, -2, -1, 0, 1, 2, 3]));
+function negative(arr) {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[i] *= -1;
+        }
+    }
+    return arr
+}
 
 // Always Hungry - Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food".  If no array values are "food", then print "I'm hungry" once.
 
+alwaysHungry([1, "food", 3, "food", 5]);
+function alwaysHungry(arr) {
+    count = null;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == "food") {
+            console.log("yummy");
+            count++;
+        }
+    }
+    if (count == null) {
+        console.log("I'm hungry")
+    }
+}
 
 // Swap Toward the Center - Given an array, swap the first and last values, third and third-to-last values, etc.  Example: swapTowardCenter([true,42,"Ada",2,"pizza"]) turns the array into ["pizza", 42, "Ada", 2, true].  swapTowardCenter([1,2,3,4,5,6]) turns the array into [6,2,4,3,5,1].  No need to return the array this time.
 
+swap([1, 2, 3, 4, 5])
+function swap(arr) {
+    for (i = 0; i < arr.length / 2; i++) {
+        if (i % 2 == 0) {
+            temp = arr[i];
+            arr[i] = arr[(arr.length - 1) - i];
+            arr[(arr.length - 1) - i] = temp;
+        }
+    }
+    console.log(arr)
+}
 
 // Scale the Array - Given an array arr and a number num, multiply all values in the array arr by the number num, and return the changed array arr.  For example, scaleArray([1,2,3], 3) should return [3,6,9].
 
+scaleArr([1, 2, 3], 3);
+function scaleArr(arr, num) {
+    for (i = 0; i < arr.length; i++) {
+        arr[i] *= num;
+    }
+    console.log(arr)
+}
