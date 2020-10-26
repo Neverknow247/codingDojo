048f36fe6e6edc551f5b39d452d319bee3771fb2
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-26 00:04:28 -0500

    fib

--- webFundamentals/JS/IntermediateFoundation/script.js
+++ webFundamentals/JS/IntermediateFoundation/script.js
@@ -33,25 +33,24 @@ function factorial(num) {
 
 // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  Do this without using recursion first.  If you don't know what a recursion is yet, don't worry as we'll be introducing this concept in Part 2 of this assignment.
 
-console.log(fib(6));
+console.log(fib(7));
 
 function fib(num) {
     if (num >= 0) {
         fib1 = 0;
-        fib2 = 0;
-        for (i = 0; i <= num; i++) {
-            if (i % 2 == 0) {
-                fib1 = fib1 + i;
-            }
-            else {
-                fib2 = fib2 + i;
-            }
+        fib2 = 1;
+        fib3 = 0;
+        while (num >= 0){
+            fib3 = fib1;
+            fib1 = fib1 +fib2;
+            fib2 = fib3;
+            num --;
         }
     }
     else {
-        return "fib(num), num must be a positive number or 0"
+        return "fib(num), num must be >= 0"
     }
-    return fib1 + fib2;
+    return fib2;
 }
 
 // Array: Second-to-Last: Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
