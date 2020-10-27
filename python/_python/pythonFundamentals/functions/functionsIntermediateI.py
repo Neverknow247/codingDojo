27484bcb763e8e69be4b4260f10e247ba7ce4a4e
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-27 16:02:55 -0500

    py

--- python/_python/pythonFundamentals/functions/functionsIntermediateI.py
+++ python/_python/pythonFundamentals/functions/functionsIntermediateI.py
@@ -13,3 +13,27 @@
 # If only a max number is provided, the function should return a random integer between 0 and the max number.
 # If only a min number is provided, the function should return a random integer between the min number and 100
 # If both a min and max number are provided, the function should return a random integer between those 2 values.
+
+# def rand(min = 0, max = 100):
+#     if min >= max && max == 100:
+
+#     if min == max:
+#         return "min"
+#     if min > max:
+#         temp = max
+#         max = min
+#         min = temp
+#     num = random.random
+#     print num
+
+import random
+import math
+def randomI(min = 0, max = 100):
+    if min > max:
+        temp = max
+        max = min
+        min = temp
+    num = random.random()*((max+1)-min)+min
+    return int(math.ceil(num))-1
+
+print(randomI(100,98))
