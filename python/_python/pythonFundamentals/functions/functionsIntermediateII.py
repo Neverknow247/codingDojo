173fe2bf0a27a876e1cd0d4a64da3ed85107ed43
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-27 22:17:31 -0500

    py

---
+++ python/_python/pythonFundamentals/functions/functionsIntermediateII.py
@@ -0,0 +1,48 @@
+x = [[5, 2, 3], [10, 8, 9]]
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'}
+]
+sports_directory = {
+    'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [{'x': 10, 'y': 20}]
+
+
+def tenToFifteen():
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            if x[i][j] == 10:
+                x[i][j] = 15
+    return x
+
+
+print(tenToFifteen())
+
+
+students = [
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+
+def iterateDictionary(_dict):
+    for i in range(len(students)):
+        names = ""
+        for key, value in students[i].items():
+            names += f"{key} - {value}"
+            if names == f"{key} - {value}":
+                names += ", "
+        print(names)
+
+
+iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
