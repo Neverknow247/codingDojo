b11cb06536ac6afae448792ca3ed7725b70f672e
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-29 15:30:51 -0500

    bankWithUsers

---
+++ python/_python/oop/usersWithBankAccounts.py
@@ -0,0 +1,38 @@
+
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        self.interest_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: ${self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += self.balance * self.interest_rate
+            self.balance = round(self.balance*100)/100
+        return self
+
+    def transfer_money(self, other, amount):
+        self.balance -= amount
+        other.balance += amount
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+    def newAccount(self,int_rate,balance):
+        return BankAccount(int_rate,balance)
+
+nathan = User("Nathan","nate@gmail.com")
+nathan.savings = nathan.newAccount(0.05,100)
+nathan.savings.deposit(200).yield_interest().display_account_info()
