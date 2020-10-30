d608a3c4b9a372e70f5908b7cb579e82c02da991
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-29 20:46:40 -0500

    py

--- python/_python/flask/flaskFundamentals/understandingRouting/server.py
+++ python/_python/flask/flaskFundamentals/understandingRouting/server.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/dojo")
+def dojo():
+    return "Dojo!"
+
+@app.route("/say/<name>")
+def hiName(name):
+    return f"Hi {name}!"
+
 if __name__ == "__main__":
     app.run(debug=True)
