fa79547b4217d05dc7b1e31ac77b25a7754842dc
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-29 20:08:34 -0500

    py

--- python/_python/flask/flaskFundamentals/helloFlask/hello.py
+++ python/_python/flask/flaskFundamentals/helloFlask/hello.py
@@ -19,6 +19,11 @@ def hello_person(name):
     print(name)
     return f"Hello {name}"
 
+@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return "username: " + username + ", id: " + id
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
