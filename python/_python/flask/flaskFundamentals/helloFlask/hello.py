533bc2a0f0cc2da093c025190ed7c5bf51f3b8e8
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-29 19:50:36 -0500

    hello

--- python/_python/flask/flaskFundamentals/helloFlask/hello.py
+++ python/_python/flask/flaskFundamentals/helloFlask/hello.py
@@ -10,4 +10,4 @@ def hello_world():
 
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-        app.run(debug=True)    # Run the app in debug mode.
+    app.run(debug=True)    # Run the app in debug mode.
