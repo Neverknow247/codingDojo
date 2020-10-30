ef201567f6074e33b2e8b4ebd7b82e12a4621d19
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-10-29 19:59:01 -0500

    hello

--- python/_python/flask/flaskFundamentals/helloFlask/hello.py
+++ python/_python/flask/flaskFundamentals/helloFlask/hello.py
@@ -11,3 +11,10 @@ def hello_world():
 
 if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
+
+
+# import statements, maybe some other routes
+@app.route('/success')
+def success():
+    return "success"
+# app.run(debug=True) should be the very last statement! 
