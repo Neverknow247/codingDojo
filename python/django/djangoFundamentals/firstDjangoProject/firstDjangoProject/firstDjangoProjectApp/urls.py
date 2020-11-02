dc9f76298951b6f17b6555a8c035e0c3a3b7b15a
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-02 14:13:32 -0600

    django

--- python/django/djangoFundamentals/firstDjangoProject/firstDjangoProject/firstDjangoProjectApp/urls.py
+++ python/django/djangoFundamentals/firstDjangoProject/firstDjangoProject/firstDjangoProjectApp/urls.py
@@ -3,6 +3,10 @@ from . import views
 urlpatterns = [
     path('', views.root),
     path('blogs', views.index),
-    path('blogs/new', views.new)
-    path('blogs/create', views.create)
+    path('blogs/new', views.new),
+    path('blogs/create', views.create),
+    path('blogs/<number>', views.show),
+    path('blogs/<int:number>/edit', views.edit),
+    path('blogs/<number>/delete', views.destroy),
+    path('blogs/json', views.json),
 ]
--- python/django/djangoFundamentals/firstDjangoProject/firstDjangoProject/firstDjangoProjectApp/views.py
+++ python/django/djangoFundamentals/firstDjangoProject/firstDjangoProject/firstDjangoProjectApp/views.py
@@ -5,5 +5,20 @@ def root(request):
 def index(request):
     return HttpResponse("placeholder to later display a list of all blogs")
 
-def new():
-    return HttpResponse()
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect("/")
+
+def show(request,number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request,number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request,number):
+    return redirect("/blogs")
+
+def json(request):
+    pass
