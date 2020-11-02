e34f18c328ea4811a13f1664fd71ed5a2571c5f7
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-02 13:46:39 -0600

    django

--- python/django/djangoFundamentals/djangoIntro/djangoIntro/introApp/urls.py
+++ python/django/djangoFundamentals/djangoIntro/djangoIntro/introApp/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('nba', views.showNbaHome),
+    path('nba/lol', views.lol),
+    path('nba/<int:val>', views.anotherMethod),
+    path('google',views.google),
+    path('nfl',views.nfl),
+    path('redir', views.redir),
 ]
--- python/django/djangoFundamentals/djangoIntro/djangoIntro/introApp/views.py
+++ python/django/djangoFundamentals/djangoIntro/djangoIntro/introApp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
 def index(request):
     return HttpResponse("this is the equivalent of @app.route('/')!.... Hello World!")
+
+def showNbaHome(request):
+    return HttpResponse("AHHHHH GOTCHA! NERD!")
+
+def lol(request):
+    return HttpResponse("check 12")
+
+def anotherMethod(request,val):
+    return HttpResponse(f"You are number {val}")
+
+def google(request):
+    return redirect("https://www.google.com/")
+
+def nfl(request):
+    return redirect("/nba")
+
+def redir(request):
+    return JsonResponse({"response": "JSON response from redirected_method", "status": True})
