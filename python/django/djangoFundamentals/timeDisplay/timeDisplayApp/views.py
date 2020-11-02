da40f60e7ea83d77b03af9f69ffdaea7a7f9004d
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-02 15:46:10 -0600

    timeDisplay

--- python/django/djangoFundamentals/timeDisplay/timeDisplayApp/views.py
+++ python/django/djangoFundamentals/timeDisplay/timeDisplayApp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
+from time import gmtime, strftime
+from datetime import date
+today = date.today()
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "date": strftime("%b %d,%Y"),
+        # "date" : today,
+        "time": strftime("%I:%M %p")
+    }
+    return render(request, "index.html", context)
 
 
