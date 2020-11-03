2ca3c96e2f2b5dc6f9be5b9b95057fada94f5e57
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-03 09:45:23 -0600

    pyScript

--- python/_python/pythonFundamentals/algos/script.py
+++ python/_python/pythonFundamentals/algos/script.py
@@ -280,6 +280,7 @@ class Node:
 class SLL:
     def __init__(self):
         self.head = None
+        self.previous = None
 
     def addToBack(self, valueInput):
         newnode = Node(valueInput)
@@ -303,12 +304,22 @@ class SLL:
             runner = runner.next
         print(result)
         return self
+    
+    def addToFront(self, valueInput):
+        if self.head != None:
+            newnode = Node(valueInput)
+            newnode.next = self.head
+            self.head.previous = newnode
+            self.head = newnode
+            return self
 
-
+    def contains(self, valueInput):
+        pass
 
 sll1 = SLL()
 
 sll1.addToBack(5).addToBack(8).addToBack(10).addToBack(20).display()
+sll1.addToFront(69).addToFront(42).display()
 
 # n1 = Node(5)
 # n2 = Node(8)
