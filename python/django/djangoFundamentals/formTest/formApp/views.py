fa926dfd0d6793619f80421e8ebca7fda2f140e9
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-04 11:18:48 -0600

    py

--- python/django/djangoFundamentals/formTest/formApp/views.py
+++ python/django/djangoFundamentals/formTest/formApp/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse
+def index(request):
+    return HttpResponse("This is the Index")
