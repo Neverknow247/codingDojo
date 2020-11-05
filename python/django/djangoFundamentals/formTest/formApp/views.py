8602eecf43d05f34d915751f7e8cd4c5a1f38054
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-04 21:28:00 -0600

    py

--- python/django/djangoFundamentals/formTest/formApp/views.py
+++ python/django/djangoFundamentals/formTest/formApp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 def index(request):
     return render(request,"index.html")
         
-def create_user(request):
+def createUser(request):
     print("Got Post Info....................")
-    print(request.POST)
+    name_from_form = request.POST['name']
+    email_from_form = request.POST['email']
+    print(name_from_form)
+    print(email_from_form)
     return render(request,"index.html")
