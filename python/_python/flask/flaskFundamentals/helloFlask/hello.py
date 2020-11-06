6784c6fefaae6f53671e064244b48fea8bc98208
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-05 20:55:27 -0600

    py

--- python/_python/flask/flaskFundamentals/helloFlask/hello.py
+++ python/_python/flask/flaskFundamentals/helloFlask/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 # Create a new instance of the Flask class called "app"
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # The "@" decorator associates this route with the function immediately following
 @app.route('/')
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return render_template('index.html', phrase="hello", times=5)  # Return the string 'Hello World!' as a response
 
 # import statements, maybe some other routes
 @app.route('/success')
