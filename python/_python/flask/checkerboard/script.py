5343020bd6157255889b88004abdf94833d0d4db
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-07 16:25:55 -0600

    chackerboard

--- python/_python/flask/checkerboard/script.py
+++ python/_python/flask/checkerboard/script.py
@@ -3,7 +3,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def eightByEight():
-    return render_template("index.html", numX= 8, numY= 8)
+    return render_template("index.html", numX= 8, numY= 8, color1= "white", color2= "black")
+
+@app.route("/<x>")
+def eightByX(x):
+    return render_template("index.html", numX= int(x), numY= 8, color1= "white", color2= "black")
+
+@app.route("/<x>/<y>")
+def XByY(x, y):
+    return render_template("index.html", numX= int(x), numY= int(y), color1= "white", color2= "black")
+
+@app.route("/<x>/<y>/<color1>/<color2>")
+def XByYColor(x, y, color1, color2):
+    return render_template("index.html", numX= int(x), numY= int(y), color1= color1, color2= color2)
 
 if __name__ == '__main__':
     app.run(debug=True)
