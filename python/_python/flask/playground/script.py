64ee317b6d74a0f6b774af23aac179e55951d01e
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-07 15:06:49 -0600

    playground

--- python/_python/flask/playground/script.py
+++ python/_python/flask/playground/script.py
@@ -3,4 +3,15 @@ app = Flask(__name__)
 
 @app.route("/play")
 def play():
-    return render_template("index.html")
+    return render_template("index.html", times= 3, color= "rgb(43, 160, 255)")
+
+@app.route("/play/<num>")
+def playX(num):
+    return render_template("index.html", times= int(num), color= "rgb(43, 160, 255)")
+
+@app.route("/play/<num>/<color>")
+def playXColor(num, color):
+    return render_template("index.html", times= int(num), color= color)
+
+if __name__ == '__main__':
+    app.run(debug=True)
