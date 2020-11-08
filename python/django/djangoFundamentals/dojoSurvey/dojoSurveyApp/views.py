a75243e257a7a3c606e7eeb0919c430412208810
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-08 14:41:23 -0600

    py

--- python/django/djangoFundamentals/dojoSurvey/dojoSurveyApp/views.py
+++ python/django/djangoFundamentals/dojoSurvey/dojoSurveyApp/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+def result(request):
+    nameFromForm = request.POST['name']
+    dojoLocation = request.POST['location']
+    favLang = request.POST['language']
+    comment = request.POST['comment']
+    context = {
+        'name' : nameFromForm,
+        'location' : dojoLocation,
+        'lang' : favLang,
+        'comment' : comment
+    }
+    return render(request, 'route.html', context)
