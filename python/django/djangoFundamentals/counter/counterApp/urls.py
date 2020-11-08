eadddc7dfed2446221d37ddd1de099ba1f00bfdb
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-08 15:45:14 -0600

    py

--- python/django/djangoFundamentals/counter/counterApp/urls.py
+++ python/django/djangoFundamentals/counter/counterApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('destroySession', views.reset)
+    path('destroySession', views.reset),
+    path('add2', views.add2)
 ]
--- python/django/djangoFundamentals/counter/counterApp/views.py
+++ python/django/djangoFundamentals/counter/counterApp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 
 def index(request):
+    request.session['total'] += 1
     request.session['counter'] += 1
     return render(request, 'index.html')
 
 def reset(request):
     request.session['counter'] = 0
+    return redirect('/')
+
+def add2(request):
+    request.session['counter'] += 1
     return redirect('/')
