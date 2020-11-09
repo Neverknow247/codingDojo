e0d08bf25ddf41c5a3fdbde3265f5e52e0ca4dfc
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-09 16:04:37 -0600

    py

--- python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/views.py
+++ python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/views.py
@@ -4,11 +4,17 @@ from .models import dojos, ninjas
 def index(request):
     context = {
         "dojoList" : dojos.objects.all()
+        # "dojoList" : dojos.objects.get(name = "Best Dojo")
     }
     return render(request, "index.html", context)
 
 def addDojo(request):
+    dojos.objects.create(name= request.POST["name"], city= request.POST["city"], state= request.POST["state"])
     return redirect('/')
 
 def addNinja(request):
+    fname = request.POST["fname"]
+    dojo = request.POST["dojo"]
+    do = dojos.objects.get(id=dojo)
+    ninjas.objects.create(firstName= fname, lastName= request.POST["lastName"], dojo= do)
     return redirect('/')
