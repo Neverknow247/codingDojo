6d0a625ab3fc506a905a9c0196ec78d17f101552
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-09 11:37:08 -0600

    dojoAndNinja

--- python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/models.py
+++ python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class dojos(models.Model):
+    name = models.CharField(max_length=45)
+    city = models.CharField(max_length=45)
+    state = models.CharField(max_length=45)
+    desc = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+class ninjas(models.Model):
+    dojo = models.ForeignKey(dojos, related_name="ninja", on_delete= models.CASCADE)
+    firstName = models.CharField(max_length=45)
+    lastName = models.CharField(max_length=45)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
--- python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/views.py
+++ python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from models import dojos, ninjas
+from .models import dojos, ninjas
 
 def index(request):
     return render(request, "index.html")
