1e4c7992180c6afd4a5347bb74c34a21e62da142
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-09 14:00:45 -0600

    py

--- python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/views.py
+++ python/django/djangoOrm/dojosAndNinjasShell/dojoAndNinjaApp/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render, redirect
 from .models import dojos, ninjas
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "dojoList" : dojos.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def addDojo(request):
+    return redirect('/')
+
+def addNinja(request):
+    return redirect('/')
