6b62bb95cd6614160ec89f3f1e32ef1bb1a59b44
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-09 01:38:49 -0600

    userTemplate

--- python/django/djangoOrm/usersWithTemplates/usersApp/views.py
+++ python/django/djangoOrm/usersWithTemplates/usersApp/views.py
@@ -8,7 +8,11 @@ def index(request):
     return render(request, "index.html", context)
 
 def addUser(request):
+    firstName = request.POST['firstName']
+    lastName = request.POST['lastName'] 
+    email = request.POST['email'] 
+    age = request.POST['age'] 
     context = {
-        
+        users.objects.create(firstName= str(firstName), lastName= str(lastName), email= str(email), age= int(age))
     }
     return redirect('/')
