afd9182e3fdd0ddaf18f58d7bad8ec309b8047f1
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-10 00:29:32 -0600

    python

--- python/django/djangoOrm/bookAuthor/bookAuthorApp/models.py
+++ python/django/djangoOrm/bookAuthor/bookAuthorApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class books(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+class authors(models.Model):
+    firstName = models.CharField(max_length=45)
+    lastName = models.CharField(max_length=45)
+    books = models.ManyToManyField(books, related_name="author")
+    notes = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
