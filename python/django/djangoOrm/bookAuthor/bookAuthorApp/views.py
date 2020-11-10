7c894df6bd832979db43ed367dcd55510bb482cc
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-10 01:39:56 -0600

    py

--- python/django/djangoOrm/bookAuthor/bookAuthorApp/views.py
+++ python/django/djangoOrm/bookAuthor/bookAuthorApp/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from .models import books, authors
 
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "books" : books.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def bookView(request, bookNum):
+    context = {
+        "book" : books.objects.get(id=bookNum)
+    }
+    return render(request, "book.html", context)
+
