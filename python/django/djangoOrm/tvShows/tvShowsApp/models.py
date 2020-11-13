c3cc619b505bfe7765ddc4b186e14ac2800b4d2c
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-13 00:36:30 -0600

    py

--- python/django/djangoOrm/tvShows/tvShowsApp/models.py
+++ python/django/djangoOrm/tvShows/tvShowsApp/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class showManager(models.Manager):
+    def basicValidator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors['title'] = "Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        if len(postData['desc']) < 10:
+            errors['desc'] = "Description should be at least 10 characters"
+
 class shows(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=255)
@@ -7,3 +17,4 @@ class shows(models.Model):
     desc = models.TextField()
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
+    objects = showManager()
