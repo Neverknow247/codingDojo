3b0c3daea72c85d6ab4dd978cc6bd6dbdf4da878
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-18 21:44:11 -0600

    py

--- python/django/djangoOrm/loginReg/loginRegApp/models.py
+++ python/django/djangoOrm/loginReg/loginRegApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class users(models.Model):
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
