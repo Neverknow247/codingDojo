93fd9194310fd9b79d337794509a604462f2d7fa
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-18 22:19:09 -0600

    py

--- python/django/djangoOrm/loginReg/loginRegApp/models.py
+++ python/django/djangoOrm/loginReg/loginRegApp/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class userManager(models.Manager):
+    def basicValidator(self, postData):
+        errors = {}
+        if len(postData['firstName']) < 2:
+            errors["firstName"] = "First name must be at least 2 character"
+        if postData['firstName'].isalpha():
+            pass
+        else:
+            errors["firstName"] = "First name must only contain letters"
+        if len(postData['lastName']) < 2:
+            errors["lastName"] = "Last name must be at least 2 characters"
+        return errors
+
 class users(models.Model):
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
@@ -7,3 +20,4 @@ class users(models.Model):
     password = models.CharField(max_length=255)
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
+    objects = userManager()
