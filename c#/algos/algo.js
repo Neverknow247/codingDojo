870c26cc6c582fe44447c8f43f72cf833758d695
Neverknow247 <71198973+Neverknow247@users.noreply.github.com>
2020-11-30 10:27:46 -0600

    py

--- c#/algos/algo.js
+++ c#/algos/algo.js
@@ -1,11 +1,25 @@
 // Write a function that returns whether the given array has a balance point between indices, where one side’s sum is equal to the other’s. Example: [1,2,3,4,10] → true (between indices 4 & 10), but [1,2,4,2,1] → false.
 
+
+
 function balance(arr) {
     sum1 = 0;
     sum2 = 0;
-    for (i = 0; i < arr.length; i++){
-        sum1 += arr[i];
+    for (j = arr.length - 1; j >= 0; j--) {
+        sum2 += arr[j]
+        console.log(sum2)
+        for (i = 0; i < j; i++) {
+            sum1 += arr[i];
+        }
+        if (sum1 == sum2) {
+            console.log(i-1);
+            console.log("true");
+            return true;
+        }
+        sum1 = 0;
     }
+    console.log("false");
+    return false;
 }
 
-balance([1, 2, 3, 4, 10])
+balance([1, 2, 2, 1, 5, 1])
