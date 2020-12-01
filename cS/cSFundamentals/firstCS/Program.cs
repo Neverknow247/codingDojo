d1ed30fa630187dfe77519df73b2a95f3792f55a
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-01 03:39:21 -0600

    cs

---
+++ cS/cSFundamentals/firstCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
