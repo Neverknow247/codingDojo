637463bbd21ec46b376c5aa54fd90be0be088241
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-01 16:48:43 -0600

    cs

---
+++ cS/cSFundamentals/fundamentalsI/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace fundamentalsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string place = "Coding Dojo";
+            Console.WriteLine(place);
+        }
+    }
+}
