585abb4357a3504156929624fe7a74fb2bf67bc6
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-01 09:09:42 -0600

    cs

--- cS/algos/algo.js
+++ cS/algos/algo.js
@@ -23,3 +23,8 @@ function balance(arr) {
 }
 
 balance([1, 2, 2, 1, 5, 1])
+
+
+// Given a sorted array and a value, return whether the array contains that value. Do not sequentially iterate the array. Instead, ‘divide and conquer’, taking advantage of the fact that the array is sorted. As always, only use built-in functions that you are prepared to recreate (write yourself) on demand!
+
+
