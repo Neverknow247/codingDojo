383f8ee5aa24ab2bc908d149e5ee9e4e330fcdf2
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-02 16:54:23 -0600

    cs

---
+++ cS/cSFundamentals/human/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle myVehicle = new Vehicle(7);
+            Console.WriteLine($"My vehicle is holding {myVehicle.numPassengers} people");
+        }
+    }
+    class Vehicle
+    {
+        public int numPassengers;
+        public Vehicle(int val)
+        {
+            numPassengers = val;
+        }
+    }
+}
