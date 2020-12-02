673b55570b5061c3b5120107e5fc1b2641b9701f
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-02 11:05:49 -0600

    cs

---
+++ cS/cSFundamentals/collectionPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace collectionPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
