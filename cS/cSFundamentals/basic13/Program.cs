0346de2cb2af01d53451c28ed76d61b0ce138584
Nathan Bell <nathanbell@Nathans-MacBook-Pro.local>
2020-12-02 18:42:58 -0600

    cs

---
+++ cS/cSFundamentals/basic13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OddArray();
+        }
+        public static int[] OddArray()
+        {
+            int count = 0;
+            for(int i = 1; i<=255; i++){
+                if(i%2==1){
+                    count++;
+                }
+            }
+            int[] arr = new int[count];
+            int temp = 0;
+            for(int i = 1; i <=255;i++){
+                if(i%2==1){
+                    arr[temp]=i;
+                    temp++;
+                }
+            }
+            foreach(int num in arr){
+                Console.WriteLine(num);
+            }
+            return arr;
+        }
+
+        // Write a function that creates, and then returns, an array that contains all the odd numbers between 1 to 255. 
+        // When the program is done, this array should have the values of [1, 3, 5, 7, ... 255].
+    }
+}
--- cS/cSFundamentals/human/Program.cs
+++ cS/cSFundamentals/human/Program.cs
@@ -6,16 +6,54 @@ namespace human
     {
         public static void Main(string[] args)
         {
-            Vehicle myVehicle = new Vehicle(7);
-            Console.WriteLine($"My vehicle is holding {myVehicle.numPassengers} people");
+            Human nathan = new Human("Nathan");
+            Human rob = new Human("Rob");
+            nathan.Attack(rob);
+            nathan.Attack(rob);
+            nathan.Attack(rob);
         }
     }
-    class Vehicle
+
+    class Human
     {
-        public int numPassengers;
-        public Vehicle(int val)
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        public int Health
         {
-            numPassengers = val;
+            get { return health; }
+        }
+        public Human(string name)
+        {
+            Name = name;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+
+        public Human(string name, int str, int intel, int dex, int hp)
+        {
+            Name = name;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dex;
+            health = hp;
+        }
+
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            Console.WriteLine($"{this.Name} has attacked {target.Name}.");
+            target.health -= this.Strength * 5;
+            Console.WriteLine($"{target.Name} has {target.health} health remaining!");
+            return target.health;
         }
     }
+
+
 }
